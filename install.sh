edf9fe7dc913499ef88410aa93f0aa301a816f13
napisani <napisani@yahoo.com>
2015-04-07 22:09:29 -0400

    small fix

--- install.sh
+++ install.sh
@@ -235,6 +235,8 @@ export TERM=xterm-256color
 
 # Set the bash prompt styling
 export PS1="\[$(tput bold)\]\[$(tput setaf 4)\][\[$(tput setaf 5)\]\u\[$(tput setaf 6)\]@\[$(tput setaf 5)\]\h \[$(tput setaf 2)\]\W\[$(tput setaf 4)\]]\\$ \[$(tput sgr0)\]"
+
+alias ls="ls --color=auto"
 EOF
 
 
