4c253f79364c38f4141a24780a3c667be9697ba2
napisani <napisani@yahoo.com>
2015-04-14 13:34:25 -0400

    added android studio to the karabiner settings

--- config/karabiner/import_profile.sh
+++ config/karabiner/import_profile.sh
@@ -6,9 +6,9 @@ $cli set remap.find_winstyle_no_term 1
 /bin/echo -n .
 $cli set remap.pcstyle_close 1
 /bin/echo -n .
-$cli set remap.select_all_winstyle_no_term 1
+$cli set remap.save_winstyle_no_term 1
 /bin/echo -n .
-$cli set remap.pc_style_open 1
+$cli set remap.fn2controlL 1
 /bin/echo -n .
 $cli set remap.undo_winstyle_no_term 1
 /bin/echo -n .
@@ -24,8 +24,10 @@ $cli set remap.pc_style_copy_paste 1
 /bin/echo -n .
 $cli set remap.new_winstyle_no_term 1
 /bin/echo -n .
-$cli set remap.save_winstyle_no_term 1
+$cli set private.ignore_ANDROID_STUDIO 1
 /bin/echo -n .
-$cli set remap.fn2controlL 1
+$cli set remap.select_all_winstyle_no_term 1
+/bin/echo -n .
+$cli set remap.pc_style_open 1
 /bin/echo -n .
 /bin/echo
